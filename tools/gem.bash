a3f7e00a07dbc818c89308211f94070873a1a90e
jrbeverly <jrbeverly@live.com>
2020-04-21 19:23:44 -0400

    Upgrade scripts for package manager types

---
+++ tools/gem.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+CONTAINER="definitions/$@"
+
+# Emit the current versions
+echo "Current versions"
+cat ${CONTAINER}/provision/gemlist
+
+mv ${CONTAINER}/provision/gemlist ${CONTAINER}/provision/gemlist.bak
+touch ${CONTAINER}/provision/gemlist
+while read line; do
+    echo "Working with ${line}"
+    input=(${line//:/ })
+    echo "Determining version for ${input}"
+    version=$(gem list "^${input}$" -r | grep -o '\((.*)\)$' | tr -d '() ' | tr ',' "\n")
+    echo "Found version as ${version}"
+    echo "${input}:${version}" >> ${CONTAINER}/provision/gemlist
+done <${CONTAINER}/provision/gemlist.bak
+
+#
+rm ${CONTAINER}/provision/gemlist.bak
+cat ${CONTAINER}/provision/gemlist
---
+++ tools/luarocks.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+CONTAINER="definitions/$@"
+
+apt update -qq
+apt install -qq luarocks -y
+
+# Emit the current versions
+echo "Current versions"
+cat ${CONTAINER}/provision/lualist
+
+mv ${CONTAINER}/provision/lualist ${CONTAINER}/provision/lualist.bak
+touch ${CONTAINER}/provision/lualist
+while read line; do
+    echo "Working with ${line}"
+    input=(${line// / })
+    echo "Determining version for ${input}"
+    luarocks install "${input}"
+    version=$(luarocks show ${input} | egrep "${input} ([0-9]{1,}\.)+[0-9]{1,}" | awk -F' ' '{print $2}')
+    echo "Found version as ${version}"
+    echo "${input} ${version}" >> ${CONTAINER}/provision/lualist
+done <${CONTAINER}/provision/lualist.bak
+
+#
+rm ${CONTAINER}/provision/lualist.bak
+cat ${CONTAINER}/provision/lualist
