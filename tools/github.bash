3c5c278dd1dcc60e59697a0f6e77ffb7667d9df3
jrbeverly <jrbeverly@live.com>
2021-01-31 18:28:51 +0000

    Cleanup old dockerfile common resources

--- tools/github.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-result=$(curl -s https://api.github.com/repos/cli/cli/releases/latest \
-    | grep "browser_download_url.*linux_amd64.deb" \
-    | cut -d : -f 2,3 \
-    | tr -d \")
