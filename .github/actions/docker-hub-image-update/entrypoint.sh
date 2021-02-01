ddb172f00c0f86374603762cad701795fe8eb305
jrbeverly <jrbeverly@live.com>
2021-02-01 23:36:42 +0000

    Workflow for upgrades

--- .github/actions/docker-hub-image-update/entrypoint.sh
+++ .github/actions/docker-hub-image-update/entrypoint.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -e
 
-if [[ -z "$GITHUB_TOKEN" ]]; then
-	echo "Set the GITHUB_TOKEN env variable."
-	exit 1
-fi
-
 if [[ -z "$REGISTRY_URL" ]]; then
 	echo "Set the REGISTRY_URL env variable."
 	exit 1
@@ -17,12 +12,12 @@ if [[ -z "$REGISTRY_IMAGE" ]]; then
 fi
 
 if [[ -z "$REGISTRY_ARCH" ]]; then
-	echo "Set the REGISTRY_IMAGE env variable."
+	echo "Set the REGISTRY_ARCH env variable."
 	exit 1
 fi
 
-if [[ -z "$REGISTRY_TRACKER" ]]; then
-	echo "Set the REGISTRY_IMAGE env variable."
+if [[ -z "$REGISTRY_TAG" ]]; then
+	echo "Set the REGISTRY_TAG env variable."
 	exit 1
 fi
 
@@ -32,7 +27,7 @@ image_digest_default=$(curl -sSL "https://registry.hub.docker.com/v2/repositorie
 )
 echo $image_digest_default
 image_digest=$(curl -sSL "https://${REGISTRY_URL}/v2/repositories/${REGISTRY_IMAGE}/tags/" | \
-	jq -r -c ".results | .[] | select( .name == \"${REGISTRY_TRACKER}\") | .images | .[] | select( .architecture == \"${REGISTRY_ARCH}\") | .digest"
+	jq -r -c ".results | .[] | select( .name == \"${REGISTRY_TAG}\") | .images | .[] | select( .architecture == \"${REGISTRY_ARCH}\") | .digest"
 )
 echo $image_digest
 if [[ -z "$image_digest" ]]; then
