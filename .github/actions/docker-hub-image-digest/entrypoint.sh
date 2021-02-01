da213ee078657fca0cb2062a4aecbafe9d4a318c
jrbeverly <jrbeverly@live.com>
2021-02-01 23:44:22 +0000

    revise to work with script only

--- .github/actions/docker-hub-image-digest/entrypoint.sh
+++ .github/actions/docker-hub-image-digest/entrypoint.sh
@@ -1,26 +1,3 @@
-#!/bin/bash
-set -e
-
-if [[ -z "$REGISTRY_URL" ]]; then
-	echo "Set the REGISTRY_URL env variable."
-	exit 1
-fi
-
-if [[ -z "$REGISTRY_IMAGE" ]]; then
-	echo "Set the REGISTRY_IMAGE env variable."
-	exit 1
-fi
-
-if [[ -z "$REGISTRY_ARCH" ]]; then
-	echo "Set the REGISTRY_ARCH env variable."
-	exit 1
-fi
-
-if [[ -z "$REGISTRY_TAG" ]]; then
-	echo "Set the REGISTRY_TAG env variable."
-	exit 1
-fi
-
 
 image_digest_default=$(curl -sSL "https://registry.hub.docker.com/v2/repositories/library/ubuntu/tags/" | \
 	jq -r -c '.results | .[] | select( .name == "focal") | .images | .[] | select( .architecture == "amd64") | .digest'
